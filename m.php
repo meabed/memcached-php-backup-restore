44f3e08318c1ce47714dbf8d88c6feefa448a9ae
romston <rvedenkin@gmail.com>
2015-09-16 19:13:43 +0300

    Help fix.

--- m.php
+++ m.php
@@ -109,7 +109,7 @@ switch ($action) {
 Example Usage : php m.php -h 127.0.0.1 -p 112112 -op restore
 -h : Memcache Host address ( default is 127.0.0.1 )
 -p : Memcache Port ( default is 11211 )
--p : Operation is required !! ( available options is : restore , backup )
+-op : Operation is required !! ( available options is : restore , backup )
 EOF;
         break;
 
