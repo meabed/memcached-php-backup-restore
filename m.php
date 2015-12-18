3b9829c10a9853e59947bab050948cc1ca6ea466
Technoboggle <Technoboggle@users.noreply.github.com>
2015-12-18 16:04:45 +0000

    Update m.php
    Added the ability to use multiple memcached servers and corrected the filename implementation

--- m.php
+++ m.php
@@ -13,10 +13,45 @@ class memcachedTools
     public $list = array();
     public $limit = 10000000000000;
 
-    public function __construct($host = '127.0.0.1', $port = '11211', $file = 'memcacheData.txt')
+    public function __construct($host = '127.0.0.1', $port = '11211')
     {
         $this->memcached = new Memcache();
-        $this->memcached->connect($host, $port);
+		$this->memcached = new Memcache();
+		// First test to see if $host is a comma seperated list
+		if(mb_strpos($host,',')===FALSE) {
+			// Just one server listed so just used that dummy
+			// check if the string contains a colon to seperate the port number
+			if(mb_strpos($host,':')===FALSE) {
+				$host_live = trim($host);
+				//set default port as none was defined
+				$port_live = (isset($port) && !empty($port)) ? (int)$port : 11211;
+			} else {
+				$temp = explode(':', $host);
+				$host_live = trim($temp[0]);
+				$port = (isset($temp[1]) && !empty($temp[1])) ? trim($temp[1]) : '';
+				// set default port value if it was empty in the defintion
+				$port_live = !empty($port) ? (int)$port : 11211;
+			}
+			$this->memcached->addServer($host_live, $port_live);
+		} else {
+			// Multiple servers assumed in MEMCACHE_HOST because a comma was found
+			$srvs = explode(',', $host);
+			foreach($srvs as $key=>$svr) {
+				// check if the string contains a colon to seperate the port number
+				if(mb_strpos($svr,':')===FALSE) {
+					$host_live = trim($svr);
+					//set default port as none was defined
+					$port_live = 11211;
+				} else {
+					$temp = explode(':', $svr);
+					$host_live = trim($temp[0]);
+					$port = (isset($temp[1]) && !empty($temp[1])) ? trim($temp[1]) : '';
+					// set default port value if it was empty in the defintion
+					$port_live = !empty($port) ? (int)$port : 11211;
+				}
+				$this->memcached->addServer($host_live, $port_live);
+			}
+		}
     }
 
     function writeKeysToFile()
@@ -91,7 +126,12 @@ foreach ($allowedArgs as $key => $val) {
     }
 
 }
-$obj = new memcachedTools($host, $port, (isset($file) ? $file : 'memcacheData.txt'));
+$obj = new memcachedTools($host, $port);
+
+// get the filename value (if pressent) and allocate to $obj->filename
+if(isset($filename) && trim($filename)!='') {
+    $obj->filename = trim($filename);
+}
 
 switch ($action) {
     case 'backup':
@@ -100,9 +140,12 @@ switch ($action) {
         echo "Memcached Data has been saved to file :" . $obj->filename;
         break;
     case 'restore':
-        $obj->writeKeysToMemcached();
-        echo "Memcached Data has been restore from file: " . $obj->filename;
-
+        $retval = $obj->writeKeysToMemcached();
+        if(!$retval) {
+            echo "Memcached Data could not be restored: " . $obj->filename . " Not Found\r\n";
+        } else {
+            echo "Memcached Data has been restore from file: " . $obj->filename . "\r\n";
+        }
         break;
     default:
         echo <<<EOF
@@ -111,6 +154,16 @@ Example Usage : php m.php -h 127.0.0.1 -p 112112 -op restore
 -p : Memcache Port ( default is 11211 )
 -op : Operation is required !! ( available options is : restore , backup )
 -f : File name (default is memcacheData.txt)
+
+NB: The -h address can now contain multiple memcache servers in a 'pool' configuration
+    these can be listed in an comma seperated list and each my optionally have a port
+    number associated with it by seperating with a colon thus: 
+        192.168.1.100:11211,192.168.1.101,192.168.1.100:11212
+    In the above example the are two physical machines but 192.168.1.100 is running two
+    instances of memcached. 
+    The servers MUST be listed here in the same order that is used to write to the pool
+    elsewhere in order for the keys to be correctly retrieved.
+
 EOF;
         break;
 
