bf3a885ba7766e8bad36fb532a6cf9fd6d9ccbf7
Mohamed Meabed <mo.meabed@gmail.com>
2018-10-27 02:45:34 +0400

    rewrite the class to support memcached, added documentation and tests, ci integration

--- .scripts/test.php
+++ .scripts/test.php
@@ -1,126 +1,47 @@
 <?php
+srand(1);
 
-class memcachedTools
-{
-    public $filename = 'memcacheData.txt';
-    public $fileMode = FILE_APPEND;
+define('INCLUDE_MODE', 1);
 
-    public $host = '127.0.0.1';
-    public $port = '11211';
+require_once __DIR__ . '/../m.php';
 
-    public $memcached = null;
+$instance = new memcachedTools();
+// this number of seconds may not exceed 60*60*24*30 (number of seconds in 30 days);
+$oneDayTime = time() + 300;
+$keyPrefix  = 'tk_';
 
-    public $list  = [];
-    public $limit = 10000000000000;
+$success   = 0;
+$notStored = 0;
+$fail      = 0;
 
-    public function __construct($host = '127.0.0.1', $port = '11211')
-    {
-        $this->memcached = new Memcached();
-        // First test to see if $host is a comma separated list
-        if (mb_strpos($host, ',') === false) {
-            // Just one server listed so just use that dummy
-            // check if the string contains a colon to separate the port number
-            if (mb_strpos($host, ':') === false) {
-                $hostLive = trim($host);
-                //set default port as none was defined
-                $portLive = (isset($port) && !empty($port)) ? (int)$port : 11211;
-            } else {
-                $temp     = explode(':', $host);
-                $hostLive = trim($temp[0]);
-                $port     = (isset($temp[1]) && !empty($temp[1])) ? trim($temp[1]) : '';
-                // set default port value if it was empty in the definition
-                $portLive = !empty($port) ? (int)$port : 11211;
-            }
-            $this->memcached->addServer($hostLive, $portLive);
-        } else {
-            // Multiple servers assumed in MEMCACHE_HOST because a comma was found
-            $servers = explode(',', $host);
-            foreach ($servers as $key => $server) {
-                // check if the string contains a colon to separate the port number
-                if (mb_strpos($server, ':') === false) {
-                    $hostLive = trim($server);
-                    //set default port as none was defined
-                    $portLive = 11211;
-                } else {
-                    $temp     = explode(':', $server);
-                    $hostLive = trim($temp[0]);
-                    $port     = (isset($temp[1]) && !empty($temp[1])) ? trim($temp[1]) : '';
-                    // set default port value if it was empty in the defintion
-                    $portLive = !empty($port) ? (int)$port : 11211;
-                }
-                $this->memcached->addServer($hostLive, $portLive);
-            }
-        }
-    }
+for ($i = 0; $i < 200; $i++) {
+    $k = md5($keyPrefix . $i . rand());
+    $r = $instance->memcached->set($k, 'test_' . rand(), $oneDayTime);
 
-    function writeKeysToFile()
-    {
-        foreach ($this->list as $row) {
-            $value = $this->memcached->get($row['key']);
-            $time  = time();
-            $data  = json_encode(
-                [
-                    'key' => $row['key'],
-                    'age' => ($row['age'] - $time),
-                    'val' => base64_encode($value),
-                ]
-            );
-            file_put_contents($this->filename, $data . PHP_EOL, $this->fileMode);
-        }
-    }
-
-    public function writeKeysToMemcached()
-    {
-        if (!is_file($this->filename)) {
-            return false;
-        }
-        $data    = file_get_contents($this->filename);
-        $allData = explode("\n", $data);
-        foreach ($allData as $key) {
-            $keyData = json_decode($key);
-            if (!isset($keyData['key'])) {
-                continue;
-            }
-            $this->memcached->set($keyData['key'], base64_decode($keyData['val']), 0, $keyData['age']);
-        }
-
-        return true;
-    }
-
-    public function getAllKeys()
-    {
-        $allSlabs = $this->memcached->getExtendedStats('slabs');
-
-        foreach ($allSlabs as $server => $slabs) {
-            foreach ($slabs as $slabId => $slabMeta) {
-                if (!is_numeric($slabId)) {
-                    continue;
-                }
-
-                $cacheDump = $this->memcached->getExtendedStats('cachedump', (int)$slabId, $this->limit);
-
-                foreach ($cacheDump as $server => $entries) {
-                    if (!$entries) {
-                        continue;
-                    }
-
-                    foreach ($entries as $eName => $eData) {
-                        $this->list[$eName] = [
-                            'key'    => $eName,
-                            'slabId' => $slabId,
-                            'size'   => $eData[0],
-                            'age'    => $eData[1],
-                        ];
-                    }
-                }
-            }
-        }
-        ksort($this->list);
+    if ($r === true) {
+        $success++;
+    } elseif ($r === Memcached::RES_NOTSTORED) {
+        $notStored++;
+    } else {
+        $fail++;
     }
 }
 
-$instance   = new memcachedTools();
-$oneDayTime = 60 * 60 * 24 * 1;
-$instance->memcached->add('test_key', 'test_value', $oneDayTime);
+sleep(2);
 
-var_dump($instance->memcached->get('test_key'));
+$slabs = $instance->getSlabs();
+$keys  = $instance->getAllKeys();
+
+echo "success count " . $success . "\n";
+echo "not stored count " . $notStored . "\n";
+echo "fail count " . $fail . "\n";
+
+echo "slabs count " . count($slabs) . "\n";
+echo "key count " . count($keys) . "\n";
+
+assert(count($keys) == 200);
+assert($success == 200);
+assert($notStored == 0);
+assert($fail == 0);
+
+echo "SUCCESS";
--- m.php
+++ m.php
@@ -1,8 +1,9 @@
 <?php
+srand(1);
 
 class memcachedTools
 {
-    public $filename = 'memcacheData.txt';
+    public $filename = __DIR__ . '/memcacheData.txt';
     public $fileMode = FILE_APPEND;
 
     public $host = '127.0.0.1';
@@ -13,48 +14,89 @@ class memcachedTools
     public $list  = [];
     public $limit = 10000000000000;
 
-    public function __construct($host = '127.0.0.1', $port = '11211')
+
+    // internals
+    private $slabs;
+    private $memcachedSock;
+    private $mcEnd = ["END\r\n", "ERROR\r\n", "STORED\r\n", "NOT_STORED\r\n", "EXISTS\r\n", "NOT_FOUND\r\n", "DELETED\r\n"];
+    private $lastEnd;
+
+
+    public function __construct($host = '127.0.0.1:11211')
     {
-        $this->memcached = new Memcached();
-        // First test to see if $host is a comma separated list
-        if (mb_strpos($host, ',') === false) {
-            // Just one server listed so just use that dummy
-            // check if the string contains a colon to separate the port number
-            if (mb_strpos($host, ':') === false) {
-                $hostLive = trim($host);
-                //set default port as none was defined
-                $portLive = (isset($port) && !empty($port)) ? (int)$port : 11211;
-            } else {
-                $temp     = explode(':', $host);
-                $hostLive = trim($temp[0]);
-                $port     = (isset($temp[1]) && !empty($temp[1])) ? trim($temp[1]) : '';
-                // set default port value if it was empty in the definition
-                $portLive = !empty($port) ? (int)$port : 11211;
+        $this->memcached = new Memcached('tools_id');
+        $this->memcached->setOption(Memcached::OPT_TCP_KEEPALIVE, 200);
+        $this->memcached->setOption(Memcached::OPT_TCP_NODELAY, true);
+        $this->memcached->setOption(Memcached::OPT_NO_BLOCK, true);
+
+        $hostList = explode(',', $host);
+        foreach ($hostList as $host) {
+            list($host, $port,) = explode(':', $host);
+            if (!$port) {
+                $port = $this->port;
             }
-            $this->memcached->addServer($hostLive, $portLive);
-        } else {
-            // Multiple servers assumed in MEMCACHE_HOST because a comma was found
-            $servers = explode(',', $host);
-            foreach ($servers as $key => $server) {
-                // check if the string contains a colon to separate the port number
-                if (mb_strpos($server, ':') === false) {
-                    $hostLive = trim($server);
-                    //set default port as none was defined
-                    $portLive = 11211;
-                } else {
-                    $temp     = explode(':', $server);
-                    $hostLive = trim($temp[0]);
-                    $port     = (isset($temp[1]) && !empty($temp[1])) ? trim($temp[1]) : '';
-                    // set default port value if it was empty in the defintion
-                    $portLive = !empty($port) ? (int)$port : 11211;
-                }
-                $this->memcached->addServer($hostLive, $portLive);
+            $this->memcached->addServer($host, $port);
+        }
+    }
+
+    // open tcp connection to server
+    // so we can run raw queries like stats cachedump, stats slabs etc...
+    // commands cheat-sheet below
+    // @link https://lzone.de/cheat-sheet/memcached
+    private function initMemcachedSock()
+    {
+        if (!is_resource($this->memcachedSock)) {
+            $this->memcachedSock = fsockopen($this->host, $this->port);
+            if (!$this->memcachedSock) {
+                die('Could not connect to memcached');
             }
         }
     }
 
+    // old memcache package had support for slab stats and cache dump to extract all keys
+    // last release is 2013 http://pecl.php.net/package-stats.php?pid=294&rid=&cid=3
+    // http://pecl.php.net/package-changelog.php?package=memcache
+    //
+    // So we switch to using memcached package
+    // http://pecl.php.net/package-changelog.php?package=memcached
+    // to send commands to memcached via TCP / like telnet, nc etc...
+    private function mcQuery($command)
+    {
+        $this->initMemcachedSock();
+        $response = '';
+        fwrite($this->memcachedSock, $command . "\r\n");
+        while (!feof($this->memcachedSock)) {
+            $chunk    = fread($this->memcachedSock, 8192);
+            $response .= $chunk;
+            $check    = substr($response, -14);
+            foreach ($this->mcEnd as $end) {
+                if (substr($check, -strlen($end)) == $end) {
+                    $response      = substr($response, 0, -strlen($end));
+                    $this->lastEnd = $end;
+                    break 2;
+                }
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * @return bool
+     */
     function writeKeysToFile()
     {
+        $filePath = __DIR__ . '/' . $this->filename;
+
+        // if file name starts with / it means its absolute path
+        if (substr($this->filename, 0, 1) === "/") {
+            $filePath = $this->filename;
+        }
+        if (!count($this->list)) {
+            echo "No data found in Memcached\n";
+
+            return true;
+        }
         foreach ($this->list as $row) {
             $value = $this->memcached->get($row['key']);
             $time  = time();
@@ -65,112 +107,172 @@ class memcachedTools
                     'val' => base64_encode($value),
                 ]
             );
-            file_put_contents($this->filename, $data . PHP_EOL, $this->fileMode);
+            file_put_contents($filePath, $data . PHP_EOL, $this->fileMode);
         }
-    }
-
-    public function writeKeysToMemcached()
-    {
-        if (!is_file($this->filename)) {
-            return false;
-        }
-        $data    = file_get_contents($this->filename);
-        $allData = explode("\n", $data);
-        foreach ($allData as $key) {
-            $keyData = json_decode($key);
-            if (!isset($keyData['key'])) {
-                continue;
-            }
-            $this->memcached->set($keyData['key'], base64_decode($keyData['val']), 0, $keyData['age']);
-        }
+        echo "SUCCESS: Memcached Data has been saved to file :" . $filePath . "\n";
 
         return true;
     }
 
+    /**
+     * @return bool
+     */
+    public function writeKeysToMemcached()
+    {
+        $filePath = __DIR__ . '/' . $this->filename;
+
+        // if file name starts with / it means its absolute path
+        if (substr($this->filename, 0, 1) === "/") {
+            $filePath = $this->filename;
+        }
+
+        if (!is_file($filePath)) {
+            echo "FAIL: Memcached Data could not be restored: " . $filePath . " Not Found\r\n";
+
+            return false;
+        }
+
+        $data    = file_get_contents($filePath);
+        $allData = explode("\n", $data);
+        foreach ($allData as $key) {
+            $keyData = json_decode($key, true);
+            if (!isset($keyData['key'])) {
+                continue;
+            }
+            $this->memcached->set($keyData['key'], base64_decode($keyData['val']), $keyData['age']);
+        }
+        echo "SUCCESS: Memcached Data has been restored from file: " . $filePath . "\r\n";
+
+        return true;
+    }
+
+    /**
+     * get all keys in memcached
+     *
+     * @return array
+     */
     public function getAllKeys()
     {
-        $allSlabs = $this->memcached->getExtendedStats('slabs');
+        $allSlabs = $this->getSlabs();
 
-        foreach ($allSlabs as $server => $slabs) {
-            foreach ($slabs as $slabId => $slabMeta) {
-                if (!is_numeric($slabId)) {
-                    continue;
-                }
+        foreach ($allSlabs as $id => $slab) {
+            $slabId = $id;
 
-                $cacheDump = $this->memcached->getExtendedStats('cachedump', (int)$slabId, $this->limit);
+            if (!is_numeric($slabId)) {
+                continue;
+            }
 
-                foreach ($cacheDump as $server => $entries) {
-                    if (!$entries) {
-                        continue;
-                    }
+            $cacheDump = $this->mcQuery('stats cachedump ' . (int)$slabId . ' ' . $this->limit);
 
-                    foreach ($entries as $eName => $eData) {
-                        $this->list[$eName] = [
-                            'key'    => $eName,
-                            'slabId' => $slabId,
-                            'size'   => $eData[0],
-                            'age'    => $eData[1],
-                        ];
-                    }
-                }
+            $re = '/ITEM\s(\w+)\s\[.*([0-9]{10})\ss\]/m';
+            preg_match_all($re, $cacheDump, $entries, PREG_SET_ORDER, 0);
+            foreach ($entries as $eData) {
+                $key = $eData[1];
+
+                $this->list[$key] = [
+                    'key'    => $key,
+                    'slabId' => $slabId,
+                    'age'    => $eData[2],
+                ];
             }
         }
-        ksort($this->list);
+
+        return $this->list;
+    }
+
+    /**
+     * get all memcached slaps
+     *
+     * @return array
+     */
+    public function getSlabs()
+    {
+        if (null !== $this->slabs) {
+            return $this->slabs;
+        }
+
+        // echo "Getting slabs stats...\n";
+        $this->slabs    = [];
+        $slabParamNames = [];
+        $res            = $this->mcQuery("stats slabs");
+
+        foreach (explode("\r\n", $res) as $line) {
+            $line = substr($line, 5);
+            $data = explode(':', $line);
+            if (count($data) == 2) {
+                $slabId                              = $data[0];
+                $slabParam                           = explode(' ', $data[1], 2);
+                $this->slabs[$slabId]['id']          = $slabId;
+                $this->slabs[$slabId][$slabParam[0]] = $slabParam[1];
+                $slabParamNames[$slabParam[0]]       = $slabParam[0];
+            }
+        }
+
+        return $this->slabs;
     }
 }
 
-$host             = '127.0.0.1';
-$port             = '11211';
-$action           = null; // defining this variable for readability only
-$allowedArguments = ['-h' => 'host', '-p' => 'port', '-op' => 'action', '-f' => 'file'];
-foreach ($allowedArguments as $key => $value) {
-    $id = array_search($key, $argv);
-    if ($id) {
-        ${$value} = isset($argv[$id + 1]) ? $argv[$id + 1] : false;
+/**
+ * Run the main cli function
+ *
+ * @param $argv
+ */
+function runCli($argv)
+{
+    $host             = '127.0.0.1';
+    $port             = '11211';
+    $action           = null; // defining this variable for readability only
+    $allowedArguments = ['-h' => 'host', '-p' => 'port', '-op' => 'action', '-f' => 'file'];
+    $argValues        = [];
+    foreach ($allowedArguments as $key => $value) {
+        $id = array_search($key, $argv);
+        if ($id) {
+            // one of the params host,port,action,file
+            ${$value} = isset($argv[$id + 1]) ? $argv[$id + 1] : false;
+        }
     }
-}
-$obj = new memcachedTools($host, $port);
+    // remove empty or nulls
+    $obj = new memcachedTools($host . ':' . $port);
 
 // get the filename value (if present) and allocate to $obj->filename
-if (isset($filename) && trim($filename) != '') {
-    $obj->filename = trim($filename);
-}
+    if (isset($file) && trim($file) != '') {
+        $obj->filename = trim($file);
+    }
 
-switch ($action) {
-    case 'backup':
-        $obj->getAllKeys();
-        $obj->writeKeysToFile();
-        echo "Memcached Data has been saved to file :" . $obj->filename;
-        break;
-    case 'restore':
-        $restore = $obj->writeKeysToMemcached();
-        if (!$restore) {
-            echo "Memcached Data could not be restored: " . $obj->filename . " Not Found\r\n";
-        } else {
-            echo "Memcached Data has been restored from file: " . $obj->filename . "\r\n";
-        }
-        break;
-    default:
-        echo <<<EOF
+    switch ($action) {
+        case 'backup':
+            $obj->getAllKeys();
+            $obj->writeKeysToFile();
+            break;
+        case 'restore':
+            $obj->writeKeysToMemcached();
+            break;
+        default:
+            echo <<<EOF
 Example Usage:
-php m.php -h 127.0.0.1 -p 112112 -op backup
-php m.php -h 127.0.0.1 -p 112112 -op restore
+php m.php -h 127.0.0.1 -p 11211 -op backup
+php m.php -h 127.0.0.1 -p 11211 -op restore
 -h : Memcache Host address ( default is 127.0.0.1 )
 -p : Memcache Port ( default is 11211 )
--op : Operation is required !! ( available options is : restore , backup )
--f : File name (default is memcacheData.txt)
+-op : Operation is required ( available options is: restore, backup )
+-f : File path (default is __DIR__.'/memcacheData.txt') relative path, or absolute path
 
 NB: The -h address can now contain multiple memcache servers in a 'pool' configuration
     these can be listed in an comma seperated list and each my optionally have a port
     number associated with it by seperating with a colon thus: 
-        192.168.1.100:11211,192.168.1.101,192.168.1.100:11212
+        192.168.1.100:11211,192.168.1.101,192.168.1.100:11211
     In the above example the are two physical machines but 192.168.1.100 is running two
     instances of memcached. 
     The servers MUST be listed here in the same order that is used to write to the pool
     elsewhere in order for the keys to be correctly retrieved.
 
 EOF;
-        break;
+            break;
 
+    }
 }
-exit;
+
+// check if the value is defined means its included or require by another file, so don't execute the cli function
+if (!defined('INCLUDE_MODE')) {
+    runCli($argv);
+}
